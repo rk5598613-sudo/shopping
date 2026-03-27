f90bd86301005738a22a18613dcb9dc7264fdf21
Rama Krishna <rk5598613@gmail.com>
2026-03-27 16:52:51 +0530

    Describe your changes

--- app.py
+++ app.py
@@ -176,13 +176,3 @@ def logout():
 if __name__ == '__main__':
     init_db()
     app.run(debug=True, port=5000)
-    import os
-from flask import Flask
-
-app = Flask(__name__)
-
-# Your routes here
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
